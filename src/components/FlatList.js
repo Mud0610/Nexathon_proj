1c8a1cd799d0c73dca6ba316b0a951835ea1a7e3
Shaikh Mudasser Ali <mudasser12@eng.rizvi.edu.in>
2024-03-23 20:12:29 +0530

    get data from json

--- src/components/FlatList.js
+++ src/components/FlatList.js
@@ -1,28 +1,39 @@
-
-import Title from "./Title"
-import FlatItem from "./FlatItem"
+import React, { useState, useEffect } from "react";
+import FlatItem from "./FlatItem";
 
 const FlatList = () => {
-    const title = {
-        text: "Lorem Ipsum",
-        description: "Lorem ipsum dolor sit ame"
-    }
-    return (
-        <section className="section-all-re">
-            <div className="container">
-                <Title title={title.text} description={title.description} />
-                <div className="row">
-                    <FlatItem slug="lorem-ipsum-1" />
-                    <FlatItem slug="lorem-ipsum-2" />
-                    <FlatItem slug="lorem-ipsum-3" />
-                    <FlatItem slug="lorem-ipsum-4" />
-                    <FlatItem slug="lorem-ipsum-5" />
-                    <FlatItem slug="lorem-ipsum-6" />  
-                </div>
-            </div>
-        </section>
-    )
+  const [flatData, setFlatData] = useState([]);
 
-}
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch(
+        "https://raw.githubusercontent.com/Mud0610/Nexathon_proj/main/src/data.json"
+      );
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      const jsonData = await response.json();
+      setFlatData(jsonData);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
+  return (
+    <section className="section-all-re">
+      <div className="container">
+        <div className="row">
+          {flatData.map((flat, index) => (
+            <FlatItem key={index} {...flat} />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export default FlatList;
